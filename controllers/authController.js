889ec9129b600ba7b90c7853dbdf91748c4850f8
Moin Sekander <moinuddin8510@gmail.com>
2022-09-22 15:33:46 -0400

    add validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,18 +5,35 @@ const knex = require('knex')(require("../knexfile"));
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
+const validator = require('validator');
+
+exports.authorize = (req, res, next) => {
+    const token = req.headers.authorization.split(' ')[1];
+  
+    jwt.verify(token, process.env.SECRET_KEY, (error, decoded) => {
+        if (error) {
+            return res.status(401).json({message: "Invalid token, please try again."});
+        }
+        req.email = decoded.email;
+        next();
+    })
+}
+
 exports.signup = (req, res) => {
     const { email, name, password, confirmPassword, address, coordinates } = req.body;
     
     if (!email || !name || !password || !confirmPassword || !address || !coordinates) {
-        return res.status(400).json({message: 'Please make sure to provide email, name, password, confirmPassword, address and coordinates fields in the request', data: req.body});
+        return res.status(400).json({message: "Please make sure to provide all fields in the request", requiredFields: ["email", "name", "password", "confirmPassword", "address", "coordinates"], data: req.body});
+    }
+    if (!validator.isEmail(email)) {
+        return res.status(400).json({message: "Please enter a valid email"});
     }
     if (password !== confirmPassword) {
         return res.status(400).json({message: "Passwords don't match, please try filling them in again"});
     }
     
     const hashedPassword = bcrypt.hashSync(password, saltRounds);
-    
+
     knex("users")
     .where({ email })
     .then((result) => {
@@ -43,7 +60,7 @@ exports.login = (req, res) => {
         const matchPasswords = bcrypt.compareSync(password, user.password, saltRounds);
         if (matchPasswords) {
             const token = jwt.sign({ email: user.email }, process.env.SECRET_KEY);
-            return res.json({token});
+            return res.json({message:"Login successful, redirecting...", token});
         } else {
             return res.status(403).json({message: "Credentials were incorrect"});
         }
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -7,25 +7,5 @@ router.post('/signup', authController.signup);
 
 router.post('/login', authController.login);
 
-// router
-//     .route("/:id")
-//         .get(async (req, res) => {
-//             const warehouseId = req.params.id;
-
-//             try {
-//                 const warehouseData = await knex
-//                 .select("*")
-//                 .from("warehouse")
-//                 .where({ id: warehouseId });
-
-//                 if (warehouseData.length === 0) {
-//                     return res.status(404).json({message: "Warehouse not found..."})
-//                 }
-
-//                 return res.json(warehouseData);
-//             } catch {
-//                 return res.status(500).json({message: "Unable to retreive warehouse data"});
-//             }
-//         })
 
 module.exports = router;
